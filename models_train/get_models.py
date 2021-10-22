2d99d07b648ac00cfe62309f9e3449a69553ca47
newmarwegner <newmarwegner@gmail.com>
2021-10-22 09:53:51 -0300

    svm

--- models_train/get_models.py
+++ models_train/get_models.py
@@ -1,8 +1,10 @@
 import os
-from pickle import dump
+from pickle import dump, load
 import geopandas as gpd
 import pandas as pd
+from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingRegressor
+from sklearn.metrics import accuracy_score, cohen_kappa_score
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
@@ -90,9 +92,10 @@ class DeepModels:
             :param Y_train: Targets
             :return: SVM Model fitted
             """
+            print('runing model svm')
             svm_ = SVC(kernel='rbf',
                        C=1,
-                       gamma=1)
+                       gamma='auto')
 
             return svm_.fit(X_train, Y_train)
 
@@ -111,15 +114,69 @@ class DeepModels:
         if not os.path.exists(dir_models):
             os.makedirs(dir_models)
 
-        models_list = ['random_forest', 'svm', 'gb']
+        # models_list = ['random_forest', 'svm', 'gb']
+        models_list = ['svm', ]
+
         for model in models_list:
             dump(locals()[model](self.X_train, self.Y_train),
                  open(f'{dir_models}/model_{model}.pkl', 'wb'))
 
+    def get_estimates(self):
+        """
+        Method to load models get predicts ands statistics
+        :return: statistics and compare between test and predicts
+        """
+        dir = os.path.join(os.getcwd(), 'models')
+        statistics = []
+        compare_tests_predicts = []
+        for root, directory, files in os.walk(dir):
+            for file in files:
+                if not file == 'model_gb.pkl':
+                    n_model = file[:-4]
+                    model = load(open(os.path.join(dir, file), 'rb'))
+                    Y_pred = model.predict(self.X_test)
+                    Y_pred_train = model.predict(self.X_train)
+                    accuracy_train = f'{n_model} Train Accuracy :: {round(accuracy_score(self.Y_train, Y_pred_train), 3)}'
+                    accuracy_test = f'{n_model} Test Accuracy  :: {round(accuracy_score(self.Y_test, Y_pred), 3)}'
+                    confusion_matrix = f'{n_model} Confussion matrix:\n {metrics.confusion_matrix(self.Y_test, Y_pred)}'
+                    report = f'{n_model} Classification report:\n {metrics.classification_report(self.Y_test, Y_pred)}'
+                    accuracy = f'{n_model} Classification accuracy: {round(metrics.accuracy_score(self.Y_test, Y_pred), 3)}'
+                    kappa = f'{n_model} Kappa accuracy:{round(cohen_kappa_score(self.Y_test, Y_pred), 3)}'
+                    statistics.append([accuracy_train, accuracy_test, confusion_matrix, report, accuracy, kappa])
+                    compare_tests_predicts.append({n_model: {'truth': self.Y_test,
+                                                             'predict': Y_pred}})
+
+        return statistics, compare_tests_predicts
+
+
+class Classification:
+    def __init__(self, raster_path, name, extension):
+        self.dpmodels = DeepModels(name, extension)
+        self.models()
+
+    def path_models(self, dir):
+        paths_models = []
+        for root, directory, files in os.walk(dir):
+            for file in files:
+                paths_models.append(os.path.join(dir, file))
+
+        return paths_models
+
+    def models(self):
+        dir = os.getcwd() + '/models'
+        paths_models = self.path_models(dir)
+        if not paths_models:
+            self.dpmodels.classifiers()
+            paths_models = self.path_models(dir)
+
+        return paths_models
 
 if __name__ == '__main__':
     mode = DeepModels('amostra', 'gpkg')
-    print(mode.classifiers())
+    mode.classifiers()
+    # for i in mode.get_estimates()[0]:
+    #     for est in i:
+    #         print(est)
     # x = mode.X_train
     # print(np.isnan(x))
     # print(np.isfinite(x))
